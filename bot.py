37020bfddb7d0e0f57c50c7a059f209c456e5350
lj <laurensjan@users.noreply.github.com>
2018-03-26 18:14:08 +0200

    First commit

---
+++ bot.py
@@ -0,0 +1,22 @@
+from telegram.ext import Updater, CommandHandler
+import os, sys
+
+TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
+if not TELEGRAM_TOKEN:
+    sys.exit('Please set environment variable TELEGRAM_TOKEN')
+
+def xkcd(bot, update):
+    update.message.reply_text(
+        'This is when I post the latest XKCD comic')
+
+def hello(bot, update):
+    update.message.reply_text(
+        'Hello {}'.format(update.message.from_user.first_name))
+
+
+updater = Updater(TELEGRAM_TOKEN)
+
+updater.dispatcher.add_handler(CommandHandler('hello', hello))
+
+updater.start_polling()
+updater.idle()
